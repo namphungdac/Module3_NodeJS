3e64ab0455d56ebde311bf7284376a75eb6c6c77
unknown <phungdacnam1997@gmail.com>
2023-05-16 14:13:42 +0700

    1

---
+++ lesson5_Template-&-Static-File/TH_Build-member-registration-form-feature/server.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const fs = require('fs');
+const qs = require('qs');
+
+const server = http.createServer((req, res) => {
+    if (req.method === 'GET') {
+        fs.readFile('./views/register.html', 'utf8', (err, data) => {
+            if (err) console.log(err);
+            else {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write(data);
+                res.end();
+            }
+        });
+    } else {
+        let dataUser = '';
+        req.on('data', chunk => {
+            dataUser += chunk;
+        });
+        req.on('end', () => {
+            console.log(qs.parse(dataUser));
+            res.end('Register success!');
+        })
+        req.on('error', () => {
+            console.log('error');
+        })
+    }
+});
+server.listen(8000, 'localhost', () => {
+    console.log('Server is running in localhost:8000');
+})
--- lesson5_Template-&-Static-File/TH_HTML-Template/server.js
+++ lesson5_Template-&-Static-File/TH_HTML-Template/server.js
@@ -7,8 +7,6 @@ const server = http.createServer((req, res) => {
             res.writeHead(404, {'Content-Type': 'text/html'});
             return res.end("404 Not Found");
         }
-        let username = 'Admin';
-        data = data.replace('{username}', username);
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
         res.end();
